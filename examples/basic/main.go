db215051d21948850511689c800ae797cff99c1f
Tim Satke <tim.satke@gmail.com>
2018-11-22 00:14:16 +0100

    Added more examples

---
+++ examples/basic/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "gitlab.com/TimSatke/abc"
+
+func main() {
+	logger := abc.NewSimpleLogger()
+	logger.SetLevel(abc.LevelDebug)
+
+	logger.Info("I'm alive!")
+
+	logger.Verbose("This will not print, since Verbose is below Debug")
+
+	logger.Fatal("This will not terminate the application")
+	logger.Debug("See? Nothing can stop me!")
+
+	logger.SetLevel(abc.LevelWarn)
+
+	logger.Error("Still printing...")
+}
---
+++ examples/file/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"gitlab.com/TimSatke/abc"
+)
+
+func main() {
+	file, _ := os.Create("my.file")
+
+	logger := abc.NewSimpleLogger()
+	logger.SetOut(io.MultiWriter(file, os.Stdout)) // writes to file and stdout
+	logger.Info("Some piece of information")
+}
