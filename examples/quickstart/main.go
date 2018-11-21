c259715476ff846fffa5aba9fac09cd52085b1cf
Tim Satke <tim.satke@gmail.com>
2018-11-22 00:01:32 +0100

    Added quickstart example

--- examples/quickstart/main.go
+++ examples/quickstart/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "gitlab.com/TimSatke/abc"
+import (
+	"fmt"
+
+	"gitlab.com/TimSatke/abc"
+)
 
 func main() {
+	abc.SetLevel(abc.LevelVerbose) // default level is INFO (abc.LevelInfo)
+
+	abc.Verbose("Hello World")             // prints as DEBG
+	abc.Verbosef("fmt: %v", "Hello World") // prints as DEBG
+
+	abc.Debug("Hello World")
+	abc.Debugf("fmt: %v", "Hello World")
+
 	abc.Info("Hello World")
-	abc.Infof("%v", "Hello World")
+	abc.Infof("fmt: %v", "Hello World")
+
+	abc.Warn("Hello World")
+	abc.Warnf("fmt: %v", "Hello World")
+
+	abc.Error("Hello World")
+	abc.Errorf("fmt: %v", "Hello World")
+
+	abc.Fatal("Hello World")
+	abc.Fatalf("fmt: %v", "Hello World")
+
+	fmt.Println("Still running")
 }
