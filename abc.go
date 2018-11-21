c894ccd2cba2a947a3d34ef4d60aa9b1079e948e
Tim Satke <tim.satke@gmail.com>
2018-11-21 23:43:11 +0100

    Added Logger functions to package

--- abc.go
+++ abc.go
@@ -42,3 +42,63 @@ func Printf(lvl LogLevel, format string, v ...interface{}) {
 		root.Printf(lvl, format, v)
 	}
 }
+
+func Inspect(v interface{}) {
+	panic("Unsupported") // TODO(TimSatke) custom implementation
+}
+
+func Verbose(v ...interface{}) {
+	Print(LevelVerbose, v...)
+}
+
+func Verbosef(format string, v ...interface{}) {
+	Printf(LevelVerbose, format, v...)
+}
+
+func Debug(v ...interface{}) {
+	Print(LevelDebug, v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	Printf(LevelDebug, format, v...)
+}
+
+func Info(v ...interface{}) {
+	Print(LevelInfo, v...)
+}
+
+func Infof(format string, v ...interface{}) {
+	Printf(LevelInfo, format, v...)
+}
+
+func Warn(v ...interface{}) {
+	Print(LevelWarn, v...)
+}
+
+func Warnf(format string, v ...interface{}) {
+	Printf(LevelWarn, format, v...)
+}
+
+func Error(v ...interface{}) {
+	Print(LevelError, v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	Printf(LevelError, format, v...)
+}
+
+func Fatal(v ...interface{}) {
+	Print(LevelFatal, v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	Printf(LevelFatal, format, v...)
+}
+
+func SetLevel(lvl LogLevel) {
+	root.SetLevel(lvl)
+}
+
+func IsLevelEnabled(lvl LogLevel) bool {
+	return root.IsLevelEnabled(lvl)
+}
