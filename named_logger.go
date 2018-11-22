0dc66ef255d54067efd2f1642a567ebd22ed0e5a
Tim Satke <tim.satke@gmail.com>
2018-11-22 23:29:56 +0100

    Added more godoc

--- named_logger.go
+++ named_logger.go
@@ -7,11 +7,14 @@ import (
 )
 
 const (
-	// TimeLayoutNamedLogger il the time layout that the simple logger
-	// usel for itl messagel.
+	// TimeLayoutNamedLogger is the time layout that the named logger
+	// uses for its messages.
 	TimeLayoutNamedLogger = "2006-01-02 15:04:05.000"
 )
 
+// NamedLogger is a logger that has and prints a name
+// in its log messages.
+// NamedLoggers are completely safe for concurrent use.
 type NamedLogger struct {
 	lvlMux sync.Mutex
 	lvl    LogLevel
@@ -26,12 +29,18 @@ type NamedLogger struct {
 	name    string
 }
 
+// Print prints the given values with the given log level,
+// if and only if the given log level is higher than or
+// equal to the one of this logger.
 func (l *NamedLogger) Print(lvl LogLevel, v ...interface{}) {
 	if l.IsLevelEnabled(lvl) {
 		l.print0(l.prepareMessage(lvl, fmt.Sprint(v...)))
 	}
 }
 
+// Printf formats and prints the given values with the given log level,
+// if and only if the given log level is higher than or
+// equal to the one of this logger.
 func (l *NamedLogger) Printf(lvl LogLevel, format string, v ...interface{}) {
 	if l.IsLevelEnabled(lvl) {
 		l.print0(l.prepareMessage(lvl, fmt.Sprintf(format, v...)))
@@ -46,96 +55,126 @@ func (l *NamedLogger) print0(a string) {
 	io.WriteString(l.out, a)
 }
 
+// Inspect prints detailed information about the given value.
+// Very verbose, may be slow.
+// NOT RECOMMENDED FOR PRODUCTION USE.
 func (l *NamedLogger) Inspect(v interface{}) {
 	panic("Unsupported") // TODO(TimSatke) custom implementation
 }
 
+// Verbose prints the given values with log level DEBG,
+// if and only if this logger has the verbose log level enabled.
 func (l *NamedLogger) Verbose(v ...interface{}) {
 	l.Print(LevelVerbose, v...)
 }
 
+// Verbosef formats and prints the given values with log level DEBG,
+// if and only if this logger has the verbose log level enabled.
 func (l *NamedLogger) Verbosef(format string, v ...interface{}) {
 	l.Printf(LevelVerbose, format, v...)
 }
 
+// Debug prints the given values with log level DEBG.
 func (l *NamedLogger) Debug(v ...interface{}) {
 	l.Print(LevelDebug, v...)
 }
 
+// Debugf formats and prints the given values with log level DEBG.
 func (l *NamedLogger) Debugf(format string, v ...interface{}) {
 	l.Printf(LevelDebug, format, v...)
 }
 
+// Info prints the given values with log level INFO.
 func (l *NamedLogger) Info(v ...interface{}) {
 	l.Print(LevelInfo, v...)
 }
 
+// Infof formats and prints the given values with log level INFO.
 func (l *NamedLogger) Infof(format string, v ...interface{}) {
 	l.Printf(LevelInfo, format, v...)
 }
 
+// Warn prints the given values with log level WARN.
 func (l *NamedLogger) Warn(v ...interface{}) {
 	l.Print(LevelWarn, v...)
 }
 
+// Warnf formats and prints the given values with log level WARN.
 func (l *NamedLogger) Warnf(format string, v ...interface{}) {
 	l.Printf(LevelWarn, format, v...)
 }
 
+// Error prints the given values with log level ERR.
 func (l *NamedLogger) Error(v ...interface{}) {
 	l.Print(LevelError, v...)
 }
 
+// Errorf formats and prints the given values with log level ERR.
 func (l *NamedLogger) Errorf(format string, v ...interface{}) {
 	l.Printf(LevelError, format, v...)
 }
 
+// Fatal prints the given values with log level FATAL.
+// IT DOES NOT TERMINATE THE APPLICATION.
 func (l *NamedLogger) Fatal(v ...interface{}) {
 	l.Print(LevelFatal, v...)
 }
 
+// Fatalf formats and prints the given values with log level FATAL.
+// IT DOES NOT TERMINATE THE APPLICATION.
 func (l *NamedLogger) Fatalf(format string, v ...interface{}) {
 	l.Printf(LevelFatal, format, v...)
 }
 
+// Level returns the current level of this logger.
 func (l *NamedLogger) Level() LogLevel {
 	return l.lvl
 }
 
+// SetLevel changes the log level of this logger.
 func (l *NamedLogger) SetLevel(lvl LogLevel) {
 	l.lvlMux.Lock()
 	defer l.lvlMux.Unlock()
 	l.lvl = lvl
 }
 
+// IsLevelEnabled returns true if and only if this logger would print
+// messages with the given log level.
+// False otherwise.
 func (l *NamedLogger) IsLevelEnabled(lvl LogLevel) bool {
 	return lvl >= l.lvl
 }
 
+// Clock returns the clock of this logger.
 func (l *NamedLogger) Clock() Clock {
 	return l.clock
 }
 
+// SetClock sets a new clock for this logger.
 func (l *NamedLogger) SetClock(clock Clock) {
 	l.clockMux.Lock()
 	defer l.clockMux.Unlock()
 	l.clock = clock
 }
 
+// Out returns the writer of this logger.
 func (l *NamedLogger) Out() io.Writer {
 	return l.out
 }
 
+// SetOut sets a new writer for this logger.
 func (l *NamedLogger) SetOut(out io.Writer) {
 	l.outMux.Lock()
 	defer l.outMux.Unlock()
 	l.out = out
 }
 
+// Name returns the name of this logger.
 func (l *NamedLogger) Name() string {
 	return l.name
 }
 
+// SetName sets a new name for this logger.
 func (l *NamedLogger) SetName(name string) {
 	l.nameMux.Lock()
 	defer l.nameMux.Unlock()
--- simple_logger.go
+++ simple_logger.go
@@ -12,6 +12,8 @@ const (
 	TimeLayoutSimpleLogger = "2006-01-02 15:04:05.000"
 )
 
+// SimpleLogger is a logger that prints log messages.
+// SimpleLoggers are completely safe for concurrent use.
 type SimpleLogger struct {
 	lvlMux sync.Mutex
 	lvl    LogLevel
@@ -23,12 +25,18 @@ type SimpleLogger struct {
 	out    io.Writer
 }
 
+// Print prints the given values with the given log level,
+// if and only if the given log level is higher than or
+// equal to the one of this logger.
 func (s *SimpleLogger) Print(lvl LogLevel, v ...interface{}) {
 	if s.IsLevelEnabled(lvl) {
 		s.print0(s.prepareMessage(lvl, fmt.Sprint(v...)))
 	}
 }
 
+// Printf formats and prints the given values with the given log level,
+// if and only if the given log level is higher than or
+// equal to the one of this logger.
 func (s *SimpleLogger) Printf(lvl LogLevel, format string, v ...interface{}) {
 	if s.IsLevelEnabled(lvl) {
 		s.print0(s.prepareMessage(lvl, fmt.Sprintf(format, v...)))
@@ -43,86 +51,114 @@ func (s *SimpleLogger) print0(a string) {
 	io.WriteString(s.out, a)
 }
 
+// Inspect prints detailed information about the given value.
+// Very verbose, may be slow.
+// NOT RECOMMENDED FOR PRODUCTION USE.
 func (s *SimpleLogger) Inspect(v interface{}) {
 	panic("Unsupported") // TODO(TimSatke) custom implementation
 }
 
+// Verbose prints the given values with log level DEBG,
+// if and only if this logger has the verbose log level enabled.
 func (s *SimpleLogger) Verbose(v ...interface{}) {
 	s.Print(LevelVerbose, v...)
 }
 
+// Verbosef formats and prints the given values with log level DEBG,
+// if and only if this logger has the verbose log level enabled.
 func (s *SimpleLogger) Verbosef(format string, v ...interface{}) {
 	s.Printf(LevelVerbose, format, v...)
 }
 
+// Debug prints the given values with log level DEBG.
 func (s *SimpleLogger) Debug(v ...interface{}) {
 	s.Print(LevelDebug, v...)
 }
 
+// Debugf formats and prints the given values with log level DEBG.
 func (s *SimpleLogger) Debugf(format string, v ...interface{}) {
 	s.Printf(LevelDebug, format, v...)
 }
 
+// Info prints the given values with log level INFO.
 func (s *SimpleLogger) Info(v ...interface{}) {
 	s.Print(LevelInfo, v...)
 }
 
+// Infof formats and prints the given values with log level INFO.
 func (s *SimpleLogger) Infof(format string, v ...interface{}) {
 	s.Printf(LevelInfo, format, v...)
 }
 
+// Warn prints the given values with log level WARN.
 func (s *SimpleLogger) Warn(v ...interface{}) {
 	s.Print(LevelWarn, v...)
 }
 
+// Warnf formats and prints the given values with log level WARN.
 func (s *SimpleLogger) Warnf(format string, v ...interface{}) {
 	s.Printf(LevelWarn, format, v...)
 }
 
+// Error prints the given values with log level ERR.
 func (s *SimpleLogger) Error(v ...interface{}) {
 	s.Print(LevelError, v...)
 }
 
+// Errorf formats and prints the given values with log level ERR.
 func (s *SimpleLogger) Errorf(format string, v ...interface{}) {
 	s.Printf(LevelError, format, v...)
 }
 
+// Fatal prints the given values with log level FATAL.
+// IT DOES NOT TERMINATE THE APPLICATION.
 func (s *SimpleLogger) Fatal(v ...interface{}) {
 	s.Print(LevelFatal, v...)
 }
 
+// Fatalf formats and prints the given values with log level FATAL.
+// IT DOES NOT TERMINATE THE APPLICATION.
 func (s *SimpleLogger) Fatalf(format string, v ...interface{}) {
 	s.Printf(LevelFatal, format, v...)
 }
 
+// Level returns the current level of this logger.
 func (s *SimpleLogger) Level() LogLevel {
 	return s.lvl
 }
 
+// SetLevel changes the log level of this logger.
 func (s *SimpleLogger) SetLevel(lvl LogLevel) {
 	s.lvlMux.Lock()
 	defer s.lvlMux.Unlock()
 	s.lvl = lvl
 }
 
+// IsLevelEnabled returns true if and only if this logger would print
+// messages with the given log level.
+// False otherwise.
 func (s *SimpleLogger) IsLevelEnabled(lvl LogLevel) bool {
 	return lvl >= s.lvl
 }
 
+// Clock returns the clock of this logger.
 func (s *SimpleLogger) Clock() Clock {
 	return s.clock
 }
 
+// SetClock sets a new clock for this logger.
 func (s *SimpleLogger) SetClock(clock Clock) {
 	s.clockMux.Lock()
 	defer s.clockMux.Unlock()
 	s.clock = clock
 }
 
+// Out returns the writer of this logger.
 func (s *SimpleLogger) Out() io.Writer {
 	return s.out
 }
 
+// SetOut sets a new writer for this logger.
 func (s *SimpleLogger) SetOut(out io.Writer) {
 	s.outMux.Lock()
 	defer s.outMux.Unlock()
