672e165861eab225c42dfddf6b6ad696b4ebedb5
Tim Satke <tim.satke@gmail.com>
2018-11-24 14:52:30 +0100

    Added benchmark(s) using no stack ops

--- custom_pattern_logger_bench_test.go
+++ custom_pattern_logger_bench_test.go
@@ -10,6 +10,21 @@ func BenchmarkCustomPatternLogger_Printf(b *testing.B) {
 		clock:   &mockClock{},
 		lvl:     LevelVerbose,
 		out:     ioutil.Discard,
+		pattern: "{{.Timestamp}} [{{.Level}}] - {{.Message}}\n",
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			logger.Printf(LevelVerbose, "formatted: %v", "some input")
+		}
+	})
+}
+func BenchmarkCustomPatternLogger_Printf_Stack_Ops(b *testing.B) {
+	logger := &CustomPatternLogger{
+		clock:   &mockClock{},
+		lvl:     LevelVerbose,
+		out:     ioutil.Discard,
 		pattern: "{{.Timestamp}} {{.File}}:{{.Line}} {{.Function}} [{{.Level}}] - {{.Message}}\n",
 	}
 	b.ReportAllocs()
