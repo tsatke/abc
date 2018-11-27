a740f4404af4d82b44122f0d74514ccdc84fbb48
Tim Satke <tim.satke@gmail.com>
2018-11-27 19:59:49 +0100

    Added more tests

--- colored_logger_test.go
+++ colored_logger_test.go
@@ -205,3 +205,81 @@ func TestColoredLogger_Print(t *testing.T) {
 		})
 	}
 }
+
+func TestColoredLogger_All_Outputs(t *testing.T) {
+	expectations := []string{
+		string(ColorGray) + "0001-01-01 00:00:00.000 [DEBG] - verbose: abc\n" + string(ColorReset),
+		string(ColorGray) + "0001-01-01 00:00:00.000 [DEBG] - verbose: fmt: abc\n" + string(ColorReset),
+		string(ColorNone) + "0001-01-01 00:00:00.000 [DEBG] - abc\n" + string(ColorReset),
+		string(ColorNone) + "0001-01-01 00:00:00.000 [DEBG] - fmt: abc\n" + string(ColorReset),
+		string(ColorGreen) + "0001-01-01 00:00:00.000 [INFO] - abc\n" + string(ColorReset),
+		string(ColorGreen) + "0001-01-01 00:00:00.000 [INFO] - fmt: abc\n" + string(ColorReset),
+		string(ColorYellow) + "0001-01-01 00:00:00.000 [WARN] - abc\n" + string(ColorReset),
+		string(ColorYellow) + "0001-01-01 00:00:00.000 [WARN] - fmt: abc\n" + string(ColorReset),
+		string(ColorRed) + "0001-01-01 00:00:00.000 [ERR ] - abc\n" + string(ColorReset),
+		string(ColorRed) + "0001-01-01 00:00:00.000 [ERR ] - fmt: abc\n" + string(ColorReset),
+		string(ColorRed) + "0001-01-01 00:00:00.000 [FATAL] - abc\n" + string(ColorReset),
+		string(ColorRed) + "0001-01-01 00:00:00.000 [FATAL] - fmt: abc\n" + string(ColorReset),
+	}
+	cnt := 0
+
+	buf := &bytes.Buffer{}
+
+	l := &SimpleLogger{
+		clock: &mockClock{},
+		lvl:   LevelVerbose,
+		out:   buf,
+	}
+
+	logger := &ColoredLogger{
+		wrapped: l,
+	}
+
+	check := func() {
+		defer func() {
+			cnt++
+		}()
+		defer buf.Reset()
+
+		if cnt >= len(expectations) {
+			panic("No more expectations")
+		}
+
+		if buf.String() != expectations[cnt] {
+			dmp := diffmatchpatch.New()
+			diffs := dmp.DiffMain(expectations[cnt], buf.String(), false)
+
+			t.Fail()
+			t.Logf(`Expected "%v"`, expectations[cnt])
+			t.Logf(`Received "%v"`, buf.String())
+			t.Logf("Diff: %v", dmp.DiffPrettyText(diffs))
+		}
+	}
+
+	// actual test flow
+
+	logger.Verbose("verbose: abc")
+	check()
+	logger.Verbosef("verbose: fmt: %v", "abc")
+	check()
+	logger.Debug("abc")
+	check()
+	logger.Debugf("fmt: %v", "abc")
+	check()
+	logger.Info("abc")
+	check()
+	logger.Infof("fmt: %v", "abc")
+	check()
+	logger.Warn("abc")
+	check()
+	logger.Warnf("fmt: %v", "abc")
+	check()
+	logger.Error("abc")
+	check()
+	logger.Errorf("fmt: %v", "abc")
+	check()
+	logger.Fatal("abc")
+	check()
+	logger.Fatalf("fmt: %v", "abc")
+	check()
+}
